fd7c34465b47e8c3b7fe5368faeff673a6be805c
ParasiagnerRe <kutakbasalik@gmail.com>
2025-09-19 11:34:10 +0300

    Update PZ14.py

---
+++ PZ14.py
@@ -0,0 +1,12 @@
+# Простое чтение и запись блока
+with open('Dostoevsky.txt', 'r', encoding='utf-8') as f:
+    text = f.read()
+
+# Извлекаем часть с 1857 годом (простой вариант)
+start = text.find('1857')
+if start != -1:
+    with open('new_file.txt', 'w', encoding='utf-8') as f:
+        f.write(text[start:])
+    print("Блок 1857 года сохранен")
+else:
+    print("1857 год не найден в тексте")
