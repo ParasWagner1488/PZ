a422f9ece043a028fd70f35b52fbe89f764fb91a
ParasiagnerRe <kutakbasalik@gmail.com>
2025-09-19 11:33:19 +0300

    Update PZ9.py

---
+++ PZ9.py
@@ -0,0 +1,40 @@
+# Англо-русский словарь (10 слов)
+words = {
+    'hello': 'привет',
+    'world': 'мир', 
+    'computer': 'компьютер',
+    'program': 'программа',
+    'language': 'язык',
+    'book': 'книга',
+    'house': 'дом',
+    'water': 'вода', 
+    'time': 'время',
+    'friend': 'друг'
+}
+
+def translate(word):
+    """Перевод слова"""
+    return words.get(word.lower(), 'Слово не найдено')
+
+def show_all():
+    """Показать все слова"""
+    print("\nСловарь:")
+    for eng, rus in words.items():
+        print(f"{eng} - {rus}")
+
+# Основная программа
+print("Англо-русский словарь")
+
+while True:
+    print("\n1 - Все слова\n2 - Перевод\n0 - Выход")
+    choice = input("Выбор: ")
+    
+    if choice == '1':
+        show_all()
+    elif choice == '2':
+        word = input("Слово: ")
+        print(f"Перевод: {translate(word)}")
+    elif choice == '0':
+        break
+    else:
+        print("Неверный выбор")
